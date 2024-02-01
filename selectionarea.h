737956de048472992cc0fc2adc3562865d4ff1a3
kawthargb <kawthar.goumaidi@gmail.com>
2024-02-01 17:32:36 +0100

    Begin of code part selectionarea with cropping model. code in comment because it doesn't work

--- selectionarea.h
+++ selectionarea.h
@@ -22,9 +22,38 @@ public:
 
     bool getIsFilter();
     void setIsFilter(bool selectBool);
+    /*//Partie copié pour rectangle
+    void drawRectSelect(QPixmap *pix);
+    void deleteRectSelect();
+    bool getIsSelect();
+    QPixmap getPixmap();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+
+public slots:
+    void selectPicture();
+    //Fin partie copié*/
 
 private:
     Ui::selectionarea *ui;
+
+    /*QPixmap *pix;
+    selectionarea *actualSelection;
+
+    QRect resizeHandle() const;
+    bool m_resizeHandlePressed = false;
+    QPoint m_mousePressOffset;
+    int m_mousePressIndex = -1;
+    QRect currentRect;
+    bool isCropping = false;
+    QPixmap cutImage();
+    QPixmap pixImage;
+    QPixmap rectToPixmap(const QRect &rect);*/
 
     QLabel *labelSelected;
     QImage originalImage;
